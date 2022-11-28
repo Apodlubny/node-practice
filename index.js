aae635a162addcdab951543913b25bfe27c5acef
Apodlubny <apodlubny@gmail.com>
2022-11-28 14:29:40 +0200

    more practice

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const { getCurrentDate } = require("../node-practice/module-1/dateUtils");
+// Module 1
+// const { getCurrentDate } = require("../node-practice/module-1/dateUtils");
 
-console.log(`current date ${getCurrentDate()}`);
+// console.log(`current date ${getCurrentDate()}`);
+
+const Calc = require("calc-js").Calc;
+console.log(process.argv);
+const [, , a, b] = process.argv;
+console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
