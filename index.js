108b1302099b59bf1f04b3938652751beca1b9f2
Apodlubny <apodlubny@gmail.com>
2022-11-28 14:52:49 +0200

    more practice

--- index.js
+++ index.js
@@ -3,7 +3,21 @@
 
 // console.log(`current date ${getCurrentDate()}`);
 
-const Calc = require("calc-js").Calc;
-console.log(process.argv);
-const [, , a, b] = process.argv;
-console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
+// const Calc = require("calc-js").Calc;
+// console.log(process.argv);
+// const [, , a, b] = process.argv;
+// console.log(new Calc(parseInt(a)).sum(parseInt(b)).finish());
+
+// path module
+// const path = require("path");
+
+// console.log(path.resolve("dateUtils.js"));
+
+// fs module
+const fs = require("fs");
+fs.readFile("./data.tx", "utf-8", (error, data) => {
+  if (error) {
+    console.error(error);
+  }
+  console.log(data);
+});
