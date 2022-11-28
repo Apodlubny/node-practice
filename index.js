ce5771721059eeeb464dcf4e317b6076a6602238
Apodlubny <apodlubny@gmail.com>
2022-11-28 13:46:44 +0200

    practice1

---
+++ index.js
@@ -0,0 +1,3 @@
+const { getCurrentDate } = require("../node-practice/module-1/dateUtils");
+
+console.log(`current date ${getCurrentDate()}`);
---
+++ module-1/dateUtils.js
@@ -0,0 +1,8 @@
+function getCurrentDate() {
+  return Date.now();
+}
+console.log(getCurrentDate());
+
+module.exports = {
+  getCurrentDate,
+};
