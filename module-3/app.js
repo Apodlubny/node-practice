9d8c6cb3b4c373c291f46dd562a187ef5661346c
Apodlubny <apodlubny@gmail.com>
2022-12-14 13:25:43 +0200

    get request

--- module-3/app.js
+++ module-3/app.js
@@ -1,5 +1,13 @@
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
+
+// Если прийдет GET запрос на адрес /contacts, выполнить эту функцию
+app.get("/contacts", (request, response) => {
+  console.log(request.url);
+  console.log(request.method);
+  console.log(request.headers);
+});
 
 app.listen(3000, () => console.log("Server running!"));
