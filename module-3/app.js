d9d61bb126c8886acce6d84130b10be0f32a95c6
Apodlubny <apodlubny@gmail.com>
2022-12-14 13:33:51 +0200

    response

--- module-3/app.js
+++ module-3/app.js
@@ -8,6 +8,10 @@ app.get("/contacts", (request, response) => {
   console.log(request.url);
   console.log(request.method);
   console.log(request.headers);
+  response.send("<h2>Contacts page</h2>");
+});
+app.get("/", (request, response) => {
+  response.send("<h1>Home page</h1>");
 });
 
 app.listen(3000, () => console.log("Server running!"));
