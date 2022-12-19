755f884775d24f00f2ab2b1b3664ae18a8eddb64
Apodlubny <apodlubny@gmail.com>
2022-12-19 19:59:01 +0200

    Update app.js

--- module-3/mongo-db/app.js
+++ module-3/mongo-db/app.js
@@ -1,24 +1,25 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
-// dotenv для скрытого доступа к паролям и секретным данным через файл .env
-// variant 1
-// const dotenv = require("dotenv");
-// dotenv.config();
-// variant 2
-require("dotenv").config();
-const { DB_HOST, PORT = 3000 } = process.env;
+const dotenv = require("dotenv");
+dotenv.config();
+// require("dotenv").config();
+
 const productsRouter = require("./routes/api/products");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/products", productsRouter);
 
-//обработка ошибки подключения к базе
+const { DB_HOST, PORT = 3000 } = process.env;
+
 mongoose
   .connect(DB_HOST)
   .then(() => app.listen(PORT))
   .catch((error) => {
-    console.log(error.message), process.exit(1);
+    console.log(error.message);
+    process.exit(1);
   });
