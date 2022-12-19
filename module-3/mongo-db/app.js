033e9eb1cdf5bd422fefb7c07fb50990ae179290
Apodlubny <apodlubny@gmail.com>
2022-12-19 18:27:46 +0200

    dotenv

--- module-3/mongo-db/app.js
+++ module-3/mongo-db/app.js
@@ -1,3 +1,17 @@
 // mongo-db user password feESIVX1Rlv1bEwx
 
 // mongodb+srv://Andy:feESIVX1Rlv1bEwx@cluster0.ics529r.mongodb.net/test
+
+// mongodb+srv://Andy:<password>@cluster0.ics529r.mongodb.net/?retryWrites=true&w=majority
+const mongoose = require("mongoose");
+// dotenv для скрытого доступа к паролям и секретным данным через файл .env
+const dotenv = require("dotenv");
+dotenv.config();
+const { DB_HOST } = process.env;
+//обработка ошибки подключения к базе
+mongoose
+  .connect(DB_HOST)
+  .then(() => console.log("Database works!"))
+  .catch((error) => {
+    console.log(error.message), process.exit(1);
+  });
