d327ff090e5660a4f56bfcaf8563d0997c3e6536
Apodlubny <apodlubny@gmail.com>
2022-12-27 11:42:34 +0200

    Update register.js

--- module-4/auth-example/controllers/auth/register.js
+++ module-4/auth-example/controllers/auth/register.js
@@ -5,8 +5,20 @@ const register = async(req, res)=>{
     const {name, email, password} = req.body;
     const user = await User.findOne({email});
     if (user){
-
+        throw new Conflict(`User with ${email} already exists`)
     }
+    const result = User.create({name, email, password})
+    res.status(201).json({
+        status: "success",
+        code: 201,
+        data: {
+            user: {
+                name,
+                email,
+            }
+        }
+
+    })
 
 }
 
