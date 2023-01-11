9b358ef9375a56aef32d2bbdc5bf33e06977e218
Apodlubny <apodlubny@gmail.com>
2023-01-11 17:02:37 +0200

    sending email algorithm

--- module-6/sendgrid/app.js
+++ module-6/sendgrid/app.js
@@ -9,3 +9,20 @@ require("dotenv").config();
 
 const { SENDGRID_API_KEY } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
+
+/* 
+создаем письмо  
+1. письмо - это объект следующей структуры
+2. отправляем письмо
+ */
+const email = {
+  to: "kelora8196@usharer.com",
+  from: "apodlubny@gmail.com",
+  subject: "Тестирование сервиса рассылки",
+  html: "<p>просто абзац с текстом</p>",
+};
+
+sgMail
+  .send(email)
+  .then(() => console.log("Email send successfully!"))
+  .catch((error) => console.log(error));
