4a0bbfba9a7c7a26681c0cbd19e6f48fb0980f85
Apodlubny <apodlubny@gmail.com>
2023-01-11 21:40:37 +0200

    nodemailer example

--- module-6/nodemailer/app.js
+++ module-6/nodemailer/app.js
@@ -13,7 +13,7 @@ const { META_PASSWORD } = process.env;
 создаем набор настроек 
 1. создаем переменную nodemailerConfig которая является набором настроек */
 
-const nodemailer = {
+const nodemailerConfig = {
   host: "smtp.meta.ua",
   port: 465, // может быть 25 иди 2255
   secure: true,
@@ -22,3 +22,22 @@ const nodemailer = {
     pass: META_PASSWORD,
   },
 };
+/* 
+созадем transporter - объект который будет отвечать непосредственно за доставку писем
+1. создаем переменную transporter и с помощью объекта настроек nodemailer и его метода createTransport передаем настройки
+2. добавляем обьект письма 
+3. отправляем с помощью transporter.send
+ */
+const transporter = nodemailer.createTransport(nodemailerConfig);
+
+const email = {
+  to: "kelora8196@usharer.com",
+  from: "baby_shark@meta.ua",
+  subject: "Тестирование сервиса рассылки",
+  html: "<p>просто абзац с текстом</p>",
+};
+
+transporter
+  .sendMail(email)
+  .then(() => console.log("Email send successfully"))
+  .catch((error) => console.log(error.message));
